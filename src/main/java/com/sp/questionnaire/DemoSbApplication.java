7e455b367da2d44db4cf371970b70ce06a5375a8
Childwanwan <1635679119@qq.com>
2018-10-17 10:21:00 +0800

    first commit

--- src/main/java/com/sp/questionnaire/DemoSbApplication.java
+++ src/main/java/com/sp/questionnaire/DemoSbApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoSbApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoSbApplication.class, args);
     }
